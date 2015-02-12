7a993031e562f718bfa488cd460c508340590f3d
samson.nguyen <samson.nguyen@solium.com>
2015-02-11 17:19:17 -0700

    Install ant and iterm2

--- modules/solium/manifests/environment.pp
+++ modules/solium/manifests/environment.pp
@@ -1,4 +1,9 @@
 class solium::environment {
   ## Install Solium specific manifests
   include solium::bash-completion
+
+  include git
+  include iterm2::dev
+  include iterm2::colors::solarized_dark
+  include solium::ant
 }
