a57be5c77b35c7391c5aafd78b941af409a76398
samson.nguyen <samson.nguyen@solium.com>
2015-02-11 22:56:11 -0700

    Add winfile (not working)

--- modules/solium/manifests/environment.pp
+++ modules/solium/manifests/environment.pp
@@ -1,10 +1,11 @@
 class solium::environment {
-  ## Install Solium specific manifests
-  include solium::bash-completion
-
   include git
   include iterm2::dev
   include iterm2::colors::solarized_dark
-  include solium::ant
   include hipchat
+
+  ## Install Solium specific manifests
+  include solium::bash-completion
+  include solium::ant
+  # include solium::winfile
 }
