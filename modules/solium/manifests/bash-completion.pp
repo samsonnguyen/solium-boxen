5e9834b5575c1df25e0d5d2a4eecb49b73b6e4ab
samson.nguyen <samson.nguyen@solium.com>
2015-02-11 22:55:48 -0700

    Remove redundant notify

--- modules/solium/manifests/bash-completion.pp
+++ modules/solium/manifests/bash-completion.pp
@@ -5,8 +5,4 @@ class solium::bash-completion {
   package { 'bash-completion':
     ensure      => latest
   }
-
-  notify { "Installed - Bash-completion":
-    require     => Package['bash-completion']
-  }
 }
