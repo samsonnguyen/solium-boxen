7d0513370ced4cc63998db3598c18be2ebf94001
samson.nguyen <samson.nguyen@solium.com>
2015-03-02 21:33:52 -0700

    Add shareworks checkout into branches

--- modules/solium/manifests/environment.pp
+++ modules/solium/manifests/environment.pp
@@ -3,11 +3,16 @@ class solium::environment {
   include iterm2::dev
   include iterm2::colors::solarized_dark
   include hipchat
+  
+  ## homebrew packages
+  $homebrew_packages = [ "coreutils" ]
+  package { $homebrew_packages: ensure => "installed" }
 
   ## Install Solium specific manifests
   include solium::bash-completion
   include solium::ant
   include solium::weblogic
+  include solium::shareworks
   include solium::sqldeveloper
   # include solium::winfile
 }
