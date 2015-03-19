8f6b49a9a9315f229fd50aa537ed022e54e0974b
samson.nguyen <samson.nguyen@solium.com>
2015-03-19 16:14:57 -0600

    Make rbenv work with Rubymin - add renameutilse

--- modules/solium/manifests/environment.pp
+++ modules/solium/manifests/environment.pp
@@ -3,9 +3,14 @@ class solium::environment {
   include iterm2::dev
   include iterm2::colors::solarized_dark
   include hipchat
-  
+
+  ## http://stackoverflow.com/questions/25790909/how-do-i-configure-boxen-and-rbenv-with-rubymine
+  exec { 'link_rbenv':
+    command => 'ln -s /opt/boxen/rbenv/ ~/.rbenv'
+  }
+
   ## homebrew packages
-  $homebrew_packages = [ "coreutils" ]
+  $homebrew_packages = [ "coreutils","renameutils" ]
   package { $homebrew_packages: ensure => "installed" }
 
   ## Install Solium specific manifests
